4bcac8f1a9c671a3292d81526be3d4f0b3fd760a
Alaa_Ebrahim247 <alaaebrahemelsayed247@gmail.com>
2019-03-26 03:44:10 +0200

    login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -8,7 +8,7 @@ class Login extends React.Component {
         return (
 <div className="container mt-5">
 <div className="row">
-<div className="col-sm-5 col-10 offset-1 col-lg-9 offset-sm-2 login-form">
+<div className="col-sm-5 col-10 offset-1 col-md-10 offset-sm-1 login-form">
 <div className="row">
 <div className="col-12 login-form__banner">
 <div className="col-auto">
@@ -35,24 +35,26 @@ class Login extends React.Component {
 <div className="row"></div>
 <div className="row"></div>
 </div>
-
 </div>
-<div className=" offset-2 row">
-<div className="col-5">
-<input class="input-checkbox" id="ckb1" type="checkbox" name="remember-me"/>
-<label class="label-checkbox" for="ckb1">Remember me</label>                            
+<div className=" offset-lg-2 mt-3 row">
+<div className="offset-lg-1 col-sm-5 checkbox-container">
+<input class="checkbox-container__input" id="ckb1" type="checkbox" name="remember-me"/>
+<label class="checkbox-container__label ml-3" for="ckb1">Remember me</label>                            
 </div>
-<div className="col-4">
+<div className="offset-1 col-sm-12  col-md-4">
 <a href="#" class="txt1"> Forgot Password?</a>
 </div>
 </div>
-<div className="row"></div>
+<div className="row">
+<div className="col-5 offset-md-3 mt-5 mb-5">
+<button className="button pb-2">Login</button>
+</div>
+</div>
+</div>
 </div>
 </div>
 
-</div>
-
-            );
-        }
-    }
+ );
+ }
+}
     export default Login;
