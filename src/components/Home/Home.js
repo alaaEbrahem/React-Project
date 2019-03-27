835a9fb91500301fbd23ac3940db9c45cd7d5dd6
RawanShaarawy <rawanshaarawyyy@gmail.com>
2019-03-28 00:24:47 +0200

    adding navigation & popular cards to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,6 +11,7 @@ class HomePage extends Component {
         return(
             <>
             <Header/>
+            {/* ---------------------------------COVER------------------------------------ */}
             <div className="cover">
                 <div className="card text-white border-0 row no-gutters">
                     {/* <img className="card-img col" src={coverimg2} alt="Card image"/> */}
@@ -20,6 +21,58 @@ class HomePage extends Component {
                         {/* <div className="row no-gutters justify-content-end"><HomeSignUp/></div> */}
                     </div>
                 </div>
+            </div>
+            {/* --------------------------------NAVIGATION BAR---------------------------------- */}
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand" href="#"> </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                    <ul class="navbar-nav">
+                    <li class="nav-item px-3">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <a class="nav-link" href="#">About us</a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <a class="nav-link" href="#">Categories</a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <a class="nav-link" href="#">Authors</a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <a class="nav-link" href="#">Terms & Conditions</a>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+            {/* --------------------------------POPULAR CARDS----------------------------------- */}
+            <div className="popular-cards container">
+            <div className="row no-gutters">
+                <div className="col-md card" >
+                    <div className="card-body">
+                        <h5 className="card-title">Popular Authors</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eius dicta, facilis repellat tempore laboriosam ad! Officiis illo consequuntur qui ipsum quidem quaerat doloremque quod ipsa nisi fugiat! Ullam, aliquam.</p>
+                        <a href="#" className="card-link">See more</a>
+                    </div>
+                </div>
+                <div className="col-md card" >
+                    <div className="card-body">
+                        <h5 className="card-title">Popular Books</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eius dicta, facilis repellat tempore laboriosam ad! Officiis illo consequuntur qui ipsum quidem quaerat doloremque quod ipsa nisi fugiat! Ullam, aliquam.</p>
+                        <a href="#" className="card-link">See more</a>
+                    </div>
+                </div>
+                <div className="col-md card" >
+                    <div className="card-body">
+                        <h5 className="card-title">Popular Categories</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eius dicta, facilis repellat tempore laboriosam ad! Officiis illo consequuntur qui ipsum quidem quaerat doloremque quod ipsa nisi fugiat! Ullam, aliquam.</p>
+                        <a href="#" className="card-link">See more</a>
+                    </div>
+                </div>
+            </div>
             </div>
             </>
         )
