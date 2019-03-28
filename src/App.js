c61a026644f104ceb591b7593b9d4ed0952b93a4
RawanShaarawy <rawanshaarawyyy@gmail.com>
2019-03-29 01:28:20 +0200

    User navbar

--- src/App.js
+++ src/App.js
@@ -7,26 +7,39 @@ import { Switch } from 'react-router';
 import AuthorProfile from './components/AuthorProfile/AuthorProfile';
 import Books from '../src/components/Books/Books';
 
-
 import Login from './components/Login/Login';
 ///////////import admin pages/////////////////////
 import Home from './components/Admin/Home/Home.js';
 
-///////////////////User Home page imports///////////////////////////////
 import HomePage from './components/Home/Home.js';
 
 ///////////////////////////////////////////////////
 import BookProfile from './components/BookProfile/BookProfile'
+
+import UserNavbar from './components/Navbar/UserNavbar/UserNavbar';
+
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// -----------------------Font Awesome Import-------------------------
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch, faUserCircle, faSignOutAlt} from '@fortawesome/free-solid-svg-icons'
 import { faHome } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faSearch,faUserCircle,faSignOutAlt)
 library.add(faHome);
+//--------------------------------------------------------------------
+
+
 const App = (props) => (
   
   <Router><>
     <Switch>
     
       <Route exact path="/" component={HomePage}/>
-    
+
+      {/* Just a Temporary route for testing */}
+      <Route exact path="/userpage" component={UserNavbar}/>
+
       <Route exact path="/author" component={AuthorProfile} />
 
       <Route exact path="/login" component={Login} />
