f0c86f965904b35ec2be168aec4494b9dda54c1c
BassantFahmy <bassantfahmy@gmail.com>
2019-03-31 19:22:51 +0200

    edit modal

--- src/components/Admin/Categories/Categories.js
+++ src/components/Admin/Categories/Categories.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Container, Modal, Button, Form } from 'react-bootstrap';
 
-import { faHome, faUsers, faBook, faAddressCard, faAlignJustify } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '../../../App.scss';
 import { MyContext } from '../../../App'
 
@@ -32,58 +30,57 @@ class Categories extends React.Component {
     const { id, name, image } = this.props.match.params
     return (
       <MyContext.Consumer>
-      {value => (
-     value.state.login? 
-   <React.Fragment>
-        <Navbar id={id} name={name} image={image} />
-        <div className="container-fluid no-gutters">
-          <div className="row no-gutters">
-            <div className="col-12 y">
-              <div className="col-lg-2  col-md-3 col-4 no-gutters">
-                <SideMenue id={id} name={name} image={image} />
-              </div>
-              <div>
+        {value => (
+          value.state.login ?
+            <React.Fragment>
+              <Navbar id={id} name={name} image={image} />
+              <div className="container-fluid no-gutters">
+                <div className="row no-gutters">
+                  <div className="col-12 y">
+                    <div className="col-lg-2  col-md-3 col-4 no-gutters">
+                      <SideMenue id={id} name={name} image={image} />
+                    </div>
+                    <div>
 
-                <Modal show={this.state.show} onHide={this.handleClose}>
-                  <Modal.Header closeButton>
-                    <Modal.Title>Add Category</Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body>
-                    <Form>
-                      <Form.Group controlId="formBasicEmail">
-                        <Form.Control type="text" placeholder="Enter category Name" />
-                      </Form.Group>
-                      <Modal.Footer>
-                        <Button variant="secondary" onClick={this.handleClose}>
-                          Cancel
-                     </Button>
-                        <Button variant="success" onClick={this.handleClose}>
-                          Add Category
-                     </Button>
-                      </Modal.Footer>
-                    </Form>
-                  </Modal.Body>
-                </Modal>
-              </div>
-              <Container>
-                <div class="text-right mt-4">
-                  <Button variant="success" onClick={this.handleShow}>
-                    Add
-                  </Button>
+                      <Modal show={this.state.show} onHide={this.handleClose}>
+                        <Modal.Header closeButton>
+                          <Modal.Title>Add Category</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                          <Form>
+                            <Form.Group controlId="formBasicEmail">
+                              <Form.Control type="text" placeholder="Enter category Name" />
+                            </Form.Group>
+                            <Modal.Footer>
+                              <Button variant="secondary" onClick={this.handleClose}>
+                                Cancel
+                              </Button>
+                              <Button variant="success" onClick={this.handleClose}>
+                                Add Category
+                              </Button>
+                            </Modal.Footer>
+                          </Form>
+                        </Modal.Body>
+                      </Modal>
+                    </div>
+                    <Container>
+                      <div class="text-right mt-4">
+                        <Button variant="success" onClick={this.handleShow}>
+                          Add
+                        </Button>
+                      </div>
+                      <Listing ></Listing>
+
+                    </Container>
+                  </div>
                 </div>
-             
-                <Listing ></Listing>
-  
-              </Container>
-            </div>
-          </div>
-        </div>
-        </React.Fragment>:""
+              </div>
+            </React.Fragment> : ""
 
-       
-)}
-</MyContext.Consumer>
-);
-}
+
+        )}
+      </MyContext.Consumer>
+    );
+  }
 }
 export default Categories;
--- src/components/Admin/Categories/Listing/ListingRow/ListingRow.js
+++ src/components/Admin/Categories/Listing/ListingRow/ListingRow.js
@@ -1,44 +1,78 @@
 import React from 'react';
-import { faEdit, faTrash, faBook, faAddressCard, faAlignJustify } from "@fortawesome/free-solid-svg-icons";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Container, Modal, Button, Form } from 'react-bootstrap';
 
 
- 
+
 
 class ListingRow extends React.Component {
-    state = {
-        data: [
-            {
-                id: 1,
-                name: "Fantasy",
-                deleted: false
-            },
-            {
-                id: 2,
-                name: "Science Fiction",
-                deleted: false
-            },
-            {
-                id: 3,
-                name: "Romance",
-                deleted: false
-            },
-            {
-                id: 4,
-                name: "Dystopia",
-                deleted: false
-            },
-        ],
-    };
+    constructor(props) {
+        super(props);
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.state = {
+            show: false,
+            data: [
+                {
+                    id: 1,
+                    name: "Fantasy",
+                    deleted: false
+                },
+                {
+                    id: 2,
+                    name: "Science Fiction",
+                    deleted: false
+                },
+                {
+                    id: 3,
+                    name: "Romance",
+                    deleted: false
+                },
+                {
+                    id: 4,
+                    name: "Dystopia",
+                    deleted: false
+                },
+            ],
+        };
+    }
+    handleClose() {
+        this.setState({ show: false });
+    }
+    handleShow() {
+        this.setState({ show: true });
+    }
+
     render() {
         return (
             this.state.data.map(R => (<tr>
                 <td>{R.id}</td>
                 <td>{R.name}</td>
                 <td>
-                <FontAwesomeIcon icon={faEdit} className="icon mr-3" />
-                <FontAwesomeIcon icon={faTrash} className="icon" />
-                
+                    <FontAwesomeIcon icon={faEdit} onClick={this.handleShow} className="icon mr-3" />
+                    <FontAwesomeIcon icon={faTrash} className="icon" />
+                    <Modal show={this.state.show} onHide={this.handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Add Category</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Control type="text" placeholder="enter new name"/>
+                                   
+                                </Form.Group>
+                                <Modal.Footer>
+                                    <Button variant="secondary" onClick={this.handleClose}>
+                                        Cancel
+                              </Button>
+                                    <Button variant="success" onClick={this.handleClose}>
+                                        Edit Name
+                              </Button>
+                                </Modal.Footer>
+                            </Form>
+                        </Modal.Body>
+                    </Modal>
                 </td>
             </tr>
             )
