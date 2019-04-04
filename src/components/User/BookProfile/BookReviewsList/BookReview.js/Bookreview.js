cafd8dae3bcb33027ba21d585b685e7194dab7ec
BassantFahmy <bassantfahmy@gmail.com>
2019-04-04 14:08:19 +0200

    static stars

--- src/components/User/BookProfile/BookReviewsList/BookReview.js/Bookreview.js
+++ src/components/User/BookProfile/BookReviewsList/BookReview.js/Bookreview.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../../../../App.scss';
 import { Col, Row } from 'react-bootstrap';
+import StaticStars from '../../../../shared/StaticStars/StaticStars';
 
 const Bookreview = (props) => (
     <>
@@ -14,28 +15,7 @@ const Bookreview = (props) => (
                     <Row>
                     <p className="ml-4 mr-2 text-success">{props.username} </p>
                     <span className="mr-3"> rated it</span>
-                    <span className="stars mr-auto" data-stars={props.ratingdesc}>
-                    <svg height="10" width="10" className="star" data-rating="1">
-                        <polygon points="4.994,0.249 6.538,3.376 9.99,3.878 7.492,6.313 8.082,9.751 4.994,8.129 1.907,9.751 
-                    2.495,6.313 -0.002,3.878 3.45,3.376 " ></polygon>
-                    </svg>
-                    <svg height="10" width="10" className="star" data-rating="2">
-                        <polygon points="4.994,0.249 6.538,3.376 9.99,3.878 7.492,6.313 8.082,9.751 4.994,8.129 1.907,9.751 
-                    2.495,6.313 -0.002,3.878 3.45,3.376 " ></polygon>
-                    </svg>
-                    <svg height="10" width="10" className="star" data-rating="3">
-                        <polygon points="4.994,0.249 6.538,3.376 9.99,3.878 7.492,6.313 8.082,9.751 4.994,8.129 1.907,9.751 
-                    2.495,6.313 -0.002,3.878 3.45,3.376 8" ></polygon>
-                    </svg>
-                    <svg height="10" width="10" className="star" data-rating="4">
-                        <polygon points="4.994,0.249 6.538,3.376 9.99,3.878 7.492,6.313 8.082,9.751 4.994,8.129 1.907,9.751 
-                    2.495,6.313 -0.002,3.878 3.45,3.376 "  ></polygon>
-                    </svg>
-                    <svg height="10" width="10" className="star" data-rating="5">
-                        <polygon points="4.994,0.249 6.538,3.376 9.99,3.878 7.492,6.313 8.082,9.751 4.994,8.129 1.907,9.751 
-                    2.495,6.313 -0.002,3.878 3.45,3.376 "  ></polygon>
-                    </svg>
-                </span>
+                    <StaticStars>{props.ratingdesc}</StaticStars> 
                 </Row>
                 <p className="greyText ml-auto">{props.reviewDate}</p>
                 <p>{props.reviewdesc}</p>
--- src/components/shared/StaticStars/StaticStars.js
+++ src/components/shared/StaticStars/StaticStars.js
@@ -1,29 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './StaticStars.scss'
 
-const staticStars=(props)=>{
-
-
-    <span className="stars" data-stars="4">
-    <svg height="10" width="10" className="star" data-rating="1">
-        <polygon points="4.994,0.249 6.538,3.376 9.99,3.878 7.492,6.313 8.082,9.751 4.994,8.129 1.907,9.751 
-    2.495,6.313 -0.002,3.878 3.45,3.376 " />
-    </svg>
-    <svg height="10" width="10" className="star" data-rating="2">
-        <polygon points="4.994,0.249 6.538,3.376 9.99,3.878 7.492,6.313 8.082,9.751 4.994,8.129 1.907,9.751 
-    2.495,6.313 -0.002,3.878 3.45,3.376 "/>
-    </svg>
-    <svg height="10" width="10" className="star" data-rating="3">
-        <polygon points="4.994,0.249 6.538,3.376 9.99,3.878 7.492,6.313 8.082,9.751 4.994,8.129 1.907,9.751 
-    2.495,6.313 -0.002,3.878 3.45,3.376 8"/>
-    </svg>
-    <svg height="10" width="10" className="star" data-rating="4">
-        <polygon points="4.994,0.249 6.538,3.376 9.99,3.878 7.492,6.313 8.082,9.751 4.994,8.129 1.907,9.751 
-    2.495,6.313 -0.002,3.878 3.45,3.376 " />
-    </svg>
-    <svg height="10" width="10" className="star" data-rating="5">
-        <polygon points="4.994,0.249 6.538,3.376 9.99,3.878 7.492,6.313 8.082,9.751 4.994,8.129 1.907,9.751 
-    2.495,6.313 -0.002,3.878 3.45,3.376 " />
-    </svg>
-    </span>
+class StaticStars extends Component {
+    render() {
+        return (
+            <span className="sstars" data-sstars={this.props.children}>
+                <svg height="10" width="10" className="sstar" data-rating="1">
+                    <polygon points="4.994,0.249 6.538,3.376 9.99,3.878 7.492,6.313 8.082,9.751 4.994,8.129 1.907,9.751 
+                2.495,6.313 -0.002,3.878 3.45,3.376 " />
+                </svg>
+                <svg height="10" width="10" className="sstar" data-rating="2">
+                    <polygon points="4.994,0.249 6.538,3.376 9.99,3.878 7.492,6.313 8.082,9.751 4.994,8.129 1.907,9.751 
+                2.495,6.313 -0.002,3.878 3.45,3.376 "/>
+                </svg>
+                <svg height="10" width="10" className="sstar" data-rating="3">
+                    <polygon points="4.994,0.249 6.538,3.376 9.99,3.878 7.492,6.313 8.082,9.751 4.994,8.129 1.907,9.751 
+                2.495,6.313 -0.002,3.878 3.45,3.376 8"/>
+                </svg>
+                <svg height="10" width="10" className="sstar" data-rating="4">
+                    <polygon points="4.994,0.249 6.538,3.376 9.99,3.878 7.492,6.313 8.082,9.751 4.994,8.129 1.907,9.751 
+                2.495,6.313 -0.002,3.878 3.45,3.376 " />
+                </svg>
+                <svg height="10" width="10" className="sstar" data-rating="5">
+                    <polygon points="4.994,0.249 6.538,3.376 9.99,3.878 7.492,6.313 8.082,9.751 4.994,8.129 1.907,9.751 
+                2.495,6.313 -0.002,3.878 3.45,3.376 " />
+                </svg>
+            </span>     
+        )
+    }
 }
-export default staticStars;
+export default StaticStars;
